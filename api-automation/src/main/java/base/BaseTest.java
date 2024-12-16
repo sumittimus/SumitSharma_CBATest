e36dd4d0d1aacac5e2dff55e59e7e85e02284801
sumittimus <mailtosumit@gmail.com>
2024-12-16 22:37:42 +1100

    Added code for -> BaseTest.java -> ConfigLoader.java Added dependencies

--- api-automation/src/main/java/base/BaseTest.java
+++ api-automation/src/main/java/base/BaseTest.java
@@ -3,7 +3,22 @@
 
 package base;
 
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.BeforeSuite;
+
+import utils.ConfigLoader;
+import utils.LoggerUtil;
+
 //Initializes global configurations and setup for all tests.
 public class BaseTest {
+	private static final Logger logger = LoggerUtil.getLogger(BaseTest.class);
+
+    @BeforeSuite
+    public void setup() {
+        logger.info("Initializing test suite setup...");
+        String baseUrl = ConfigLoader.get("base.url");
+        logger.info("Base URL loaded from config: " + baseUrl);
+    }
+
 
 }
--- api-automation/src/main/java/utils/ConfigLoader.java
+++ api-automation/src/main/java/utils/ConfigLoader.java
@@ -1,7 +1,25 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.util.Properties;
 
 //Loads properties from config.properties for configuration management.
 public class ConfigLoader {
+	
+	private static Properties properties;
+
+    static {
+        properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/main/resources/config.properties")) {
+            properties.load(fis);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load config.properties");
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+
 
 }
